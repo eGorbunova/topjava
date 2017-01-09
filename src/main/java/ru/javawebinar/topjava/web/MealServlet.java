aecfe0340f6afaa77c6c4560918544f01a418f82
eGorbunova <gorbunova.kate29@gmail.com>
2017-01-09 23:52:02 +0300

    1 7 remote jmx

---
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -0,0 +1,25 @@
+package ru.javawebinar.topjava.web;
+
+import org.slf4j.Logger;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Created by Work on 09.01.2017.
+ */
+public class MealServlet extends HttpServlet {
+    private static final Logger LOG = getLogger(MealServlet.class);
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOG.debug("redirect to meals");
+
+//        request.getRequestDispatcher("/users.jsp").forward(request, response);
+        response.sendRedirect("meals.jsp");
+    }
+}
---
+++ src/main/webapp/meals.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Work
+  Date: 09.01.2017
+  Time: 23:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meal list</title>
+</head>
+<body>
+<h2><a href="index.html">Home</a></h2>
+<h2>Meal list</h2>
+</body>
+</html>
